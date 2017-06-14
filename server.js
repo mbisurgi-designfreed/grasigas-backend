cccbb7be7229e21ccfaf4241461411a694de14af
mbisurgi <maximiliano@designfreed.com>
2017-06-14 13:38:36 -0300

    Add CRUD api for Movimientos

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var mongodb = require('mongodb');
 var parser = require('body-parser');
 
 var app = express();
+var ObjectID = mongodb.ObjectID;
 var db;
 
 mongodb.MongoClient.connect('mongodb://localhost:27017/grasi-gas', (err, database) => {
@@ -120,6 +121,101 @@ app.delete('/api/choferes/:dni', (req, res) => {
     var dni = req.params.dni;
 
     db.collection('choferes').deleteOne({dni: dni}, (err, doc) => {
+        if (err) {
+            res.statusCode = 500;
+
+            var e = {
+                code: err.code,
+                name: err.name,
+                message: err.message,
+                stacktrace: err.stack
+            }
+
+            res.json(e);
+        } else {
+            res.statusCode = 200;
+            res.json(doc);
+        }
+    });
+});
+
+app.get('/api/movimientos', (req, res) => {
+    var desde = req.query.desde
+    var hasta = req.query.hasta;
+
+    db.collection('movimientos').find({fecha: {$gte: desde, $lte: hasta}}).toArray((err, docs) => {
+        if (err) {
+            res.statusCode = 500;
+
+            var e = {
+                code: err.code,
+                name: err.name,
+                message: err.message,
+                stacktrace: err.stack
+            }
+
+            res.json(e);
+        } else {
+            res.statusCode = 200;
+            res.json(docs);
+        }
+    });
+});
+
+app.post('/api/movimientos', (req, res) => {
+    req.body.choferId = new ObjectID(req.body.choferId);
+    
+    var newMovimiento = req.body;
+
+    db.collection('movimientos').insertOne(newMovimiento, (err, r) => {
+        if (err) {
+            res.statusCode = 500;
+
+            var e = {
+                code: err.code,
+                name: err.name,
+                message: err.message,
+                stacktrace: err.stack
+            }
+
+            res.json(e);
+        } else {
+            res.statusCode = 201;
+            res.json(r);
+        }
+    });
+});
+
+app.get('/api/movimientos/:choferId', (req, res) => {
+    var id = new ObjectID(req.params.choferId);
+    var desde = req.query.desde;
+    var hasta = req.query.hasta;
+
+    db.collection('movimientos').find({fecha: {$gte: desde, $lte: hasta}, choferId: id}).toArray((err, docs) => {
+        if (err) {
+            res.statusCode = 500;
+
+            var e = {
+                code: err.code,
+                name: err.name,
+                message: err.message,
+                stacktrace: err.stack
+            }
+
+            res.json(e);
+        } else {
+            res.statusCode = 200;
+            res.json(docs);
+        }
+    });
+});
+
+app.put('/api/movimientos/:choferId', (req, res) => {
+    var id = new ObjectID(req.params.choferId);
+    var fecha = req.body.fecha;
+    var updatedMovimiento = req.body;
+
+    db.collection('movimientos').updateOne({fecha: fecha, choferId: id}, {$set: updatedMovimiento}, (err, doc) => {
         if (err) {
             res.statusCode = 500;
 
